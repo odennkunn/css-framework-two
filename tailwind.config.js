e02cd7d33b0b1461bc71e778fde30235b454337e
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2020-10-14 11:07:59 +0900

    tailwind test
    test

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
